2ffeadf72f95dfdb8ddc9172ff546dc956d3a522
jeongdawoont-star <vibe1@kshcm.net>
2026-05-28 08:30:01 +0700

    feat: add Patch ChungYeok hotfix route

---
+++ Patch/ChungYeok/patch-1.0.2.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  if (window.__CHUNGYEOK_PATCH_102_APPLIED__) return;
+  window.__CHUNGYEOK_PATCH_102_APPLIED__ = true;
+
+  console.info('[CHUNGYEOK PATCH] patch-1.0.2 loaded');
+})();
