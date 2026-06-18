c1d5cbbb105e0ae0a8a5ea7b2bf7a079a52ef971
Hashbrown <jeongdawoont@gmail.com>
2026-06-18 20:16:40 +0700

    Add files via upload

---
+++ vibe-apps/밴드공연 악보/band-songbook-data.js
@@ -0,0 +1,639 @@
+window.BAND_SONGBOOK_DEFAULT_GROUPS = {
+  '연습곡': {
+    songs: [
+      {
+        id: 'day6_page_default',
+        title: '한 페이지가 될 수 있게 (DAY6)',
+        meta: 'Capo 1 | Key D | BPM 130',
+        content:
+`[INTRO]
+Gadd2  Asus4  Bm7  D
+Gadd2  Asus4  D
+Gadd2  Asus4  Bm7  D
+Gadd2  Asus4  D
+
+[INTRO 2]
+Gadd2  Asus4  Bm7  D
+Gadd2  Asus4  D
+Gadd2  Asus4  Bm7  D
+Gadd2  Asus4  D
+                 솔직히
+
+[VERSE 1]
+Gadd2
+말한 게 많이 기다려 왔어
+Gadd2                   D/F#
+너도 그랬을 거라 믿어 오늘이 오
+D/F#                    Em7
+길 매일같이 달력을 보면서
+Em7                 D             Em7  D/F#
+나도 목 빠져라 기다렸어 솔직히
+Gadd2
+나에게도 지금 이 순간은
+Gadd2                   D/F#
+꿈만 같아 너와 함께라 오늘을 위
+D/F#                    Em7
+해 꽤 많은 걸 준비해 봤어
+Em7    Asus4        D             Em7  D/F#
+그저 견뎌줘서 고마워 All
+
+[PRE-CHORUS 1]
+Bm7              Csus4  C      Dadd2
+abc-o-o-out you and I   다른 건 다 제쳐두고
+Bm7              Csus4  C      Gadd2  D/A  Asus4
+No-o-o-ow come with me  Take my hand
+
+[CHORUS 1]
+D/F#        Bm7        Em7        F#7
+아름다운 청춘의 한 장 함께 써 내려가자
+Bm7       D/F#        Gadd2      Asus4
+너와의 추억들로 가득 채울래 (come on!)
+D/F#        Bm7        Em7        F#7
+아무 걱정도 하지는 마 나에게 다 맡겨봐
+Bm7       A            Gadd2       D/F#
+지금 이 순간이 다시 넘겨볼 수 있는
+Em7       A
+한 페이지가 될 수 있게
+
+[INTRO 3]
+Gadd2  Asus4  Bm7  D
+Gadd2  Asus4  D
+                 솔직히
+
+[VERSE 2]
+Gadd2
+말할 게 지금이 오기까지
+Gadd2                   D/F#
+마냥 순탄하진 않았지 오늘이 오
+D/F#                    Em7
+길 나도 목 빠져라 기다렸어
+Em7                 D             Em7  D/F#
+나도 목 빠져라 기다렸어 솔직히
+Gadd2
+나보다도 네가 몇 배는 더
+Gadd2                   D/F#
+힘들었을 거라고 믿어 오늘을 위
+D/F#                    Em7
+해 그저 견뎌줘서 고마워
+Em7    Asus4        D             Em7  D/F#
+그저 견뎌줘서 고마워 All
+
+[PRE-CHORUS 2]
+Bm7              Csus4  C      Dadd2
+abc-o-o-out you and I   다른 건 다 제쳐두고
+Bm7              Csus4  C      Gadd2  D/A  Asus4
+No-o-o-ow come with me  Take my hand
+
+[CHORUS 2]
+D/F#        Bm7        Em7        F#7
+아름다운 청춘의 한 장 함께 써 내려가자
+Bm7       D/F#        Gadd2      Asus4
+너와의 추억들로 가득 채울래 (come on!)
+D/F#        Bm7        Em7        F#7
+아무 걱정도 하지는 마 나에게 다 맡겨봐
+Bm7       A            Gadd2       D/F#
+지금 이 순간이 다시 넘겨볼 수 있는
+Em7       A
+한 페이지가 될 수 있게
+
+[BRIDGE]
+Gadd2      Asus4        Bm7        D
+Want you to             come on out and have fun
+Gadd2      Asus4        D
+Gadd2      Asus4        Bm7        D
+Want us to              have the time of our life
+Gadd2      Asus4        D
+
+[CHORUS 3]
+D/F#        Bm7        Em7        F#7
+아 -
+Bm7       D/F#        Gadd2      Asus4
+너와의 추억들로 가득 채울래 (come on!)
+D/F#        Bm7        Em7        F#7
+아무 걱정도 하지는 마 나에게 다 맡겨봐
+Bm7       A            Gadd2       D/F#
+지금 이 순간이 다시 넘겨볼 수 있는
+Em7       A
+한 페이지가 될 수 있게
+
+[OUTRO]
+Gadd2      Asus4        Bm7        D
+This is our page our page
+Gadd2      Asus4        D
+Gadd2      Asus4        Bm7        D
+This is our page our page
+Gadd2      Asus4        D          D`
+      },
+      {
+        id: 'seasons_song_default',
+        title: 'Seasons Song',
+        meta: 'Key G | BPM 120',
+        ytUrl: 'https://www.youtube.com/watch?v=ksGiLaIx39c',
+        content:
+`[CHORUS]
+G            Gbm       Em       D
+There are four seasons that happen in a year
+C              Em 
+Spring, Summer, Autumn, Winter
+C              D
+Spring, Summer, Autumn, Winter
+G            Gbm       Em       D
+There are four seasons that happen in a year
+C              Em 
+Spring, Summer, Autumn, Winter
+C              D
+Spring, Summer, Autumn, Winter
+
+[VERSE]
+G         Gbm           Em       D
+Can you tell me what season it is?  Let's take a look
+
+G   Gbm  Em  D
+    SPRING
+G         Gbm           Em       D
+Can you tell me what season it is?  Let's take a look
+G   Gbm  Em  D
+    SUMMER
+G         Gbm           Em       D
+Can you tell me what season it is?  Let's take a look
+G   Gbm  Em  D
+    AUTUMN
+G         Gbm           Em       D
+Can you tell me what season it is?  Let's take a look
+G   Gbm  Em  D
+    WINTER
+
+// Please repeat one more time
+
+[CHORUS 2]
+G            Gbm       Em       D
+There are four seasons that happen in a year
+C              Em 
+Spring, Summer, Autumn, Winter
+C              D
+Spring, Summer, Autumn, Winter
+G            Gbm       Em       D
+There are four seasons that happen in a year
+C              Em 
+Spring, Summer, Autumn, Winter
+C              D
+Spring, Summer, Autumn, Winter`
+      },
+      {
+        id: 'shapes_song_default',
+        title: 'Shapes',
+        meta: 'Key A',
+        ytUrl: 'https://www.youtube.com/watch?v=dsR0h50BiFQ',
+        content:
+`[VERSE]
+A                         D
+Shapes are everywhere you look
+F#m        E             D
+Everywhere you look there are shapes
+A                         D
+Shapes are found in everything
+F#m        E             D
+Everything is made up of shapes
+E
+Come on! Let's find shapes!
+
+[SHAPES]
+D          A
+I see a circle, I see a square
+E
+I see a triangle over there
+E                         A
+Come on kids, let's draw shapes in the air
+A        D      A E
+SQUARE, TRIANGLE, CIRCLE!
+E
+FASTER!
+A
+YEAH!`
+      },
+      {
+        id: 'planets_solar_system_default',
+        title: 'The Planets (Solar System Song)',
+        meta: 'Pancake Manor | Key F | Approx. chords | starts 0:27',
+        ytUrl: 'https://www.youtube.com/watch?v=tNlhqPz5-Kw&t=27s',
+        content:
+`// Approx. chords converted from chart: Fa/Sib/Rem/Do = F/Bb/Dm/C
+// Video song starts around 0:27
+
+[INTRO]
+F  Bb  Dm  C
+F  Bb  Dm  C
+
+[VERSE 1]
+F
+We live in a solar system
+Bb
+In the centre is the Sun
+Dm                       C
+All the planets move around it
+Bb                       C
+We've seen every single one
+Dm               F
+Orbiting the big, bright sun
+Bb          C    F
+Orbiting the big, bright sun
+
+[VERSE 2]
+F
+Planets in our solar system
+Bb
+1,2,3,4,5,6,7,8
+Dm
+Telescopes, they help us see them
+C
+We've seen every single one
+Dm               F
+Orbiting the big, bright sun
+Bb          C    F
+Orbiting the big, bright sun
+
+[PLANETS]
+A             D
+Mercury, Venus and Earth and Mars
+A             D
+And Jupiter and Saturn
+A             D
+Uranus too, and don't forget
+A             D
+That pretty blue Neptune
+
+[VERSE 3]
+F
+We live in a solar system
+Bb
+In the centre is the Sun
+Dm                       C
+All the planets move around it
+Bb                       C
+We've seen every single one
+Dm               F
+Orbiting the big, bright sun
+Bb          C    F
+Orbiting the big, bright sun`
+      },
+      {
+        id: 'pancake_party_default',
+        title: 'Pancake Party',
+        meta: 'Pancake Manor | Key F | BPM 150 | Approx. chords',
+        ytUrl: 'https://www.youtube.com/watch?v=XDSDaFhOuo4',
+        content:
+`// Approx. chords based on chart overview
+// Main chords: F, Bb, C, A, Am
+
+[INTRO]
+F  Bb  A  C
+F  Bb  A  C
+
+[VERSE 1]
+F
+It's now Saturday
+Bb             A        C        F
+Wake up, wipe the sleep from your eyes
+F
+It's our favourite day
+Bb             A         C             F
+Mama's gonna make us a treat when we rise
+
+[STEPS]
+Bb
+STEP ONE - You pour it
+F              C
+STEP TWO - You flip it
+Bb                 F
+STEP THREE - You stack it, higher and higher
+
+[CHORUS]
+C             F
+It's a Pancake Party
+C              F
+What a way to wake up
+Bb            F
+It's a Pancake Party
+C
+Pour that syrup
+
+[TOPPINGS]
+Bb           Am    C     F
+Strawberry, Banana
+Bb           Am    C     F
+Peanut Butter, Chocolate Chips
+Bb           A     C
+Blueberry, Fish
+C
+(Just kidding)
+
+[STEPS 2]
+Bb
+STEP ONE - You pour it
+F              C
+STEP TWO - You flip it
+Bb                 F
+STEP THREE - You stack it, higher and higher
+
+[CHORUS 2]
+C             F
+It's a Pancake Party
+C              F
+What a way to wake up
+Bb            F
+It's a Pancake Party
+C             F
+Pour that syrup`
+      },
+      {
+        id: 'apt_rose_default',
+        title: 'APT. (ROSE)',
+        meta: 'Key Eb | Tuning E A D G B E | Intermediate',
+        content:
+`[INTRO]
+N.C.
+Chaeyeongiga joahaneun random game
+N.C.
+Random game
+N.C.
+Game start
+
+[CHORUS]
+N.C.
+Apateu, apateu
+N.C.
+Apateu, apateu
+N.C.
+Apateu, apateu
+N.C.
+Uh, uh-huh, uh-huh
+N.C.
+Apateu, apateu
+N.C.
+Apateu, apateu
+N.C.
+Apateu, apateu
+N.C.
+Uh, uh-huh, uh-huh
+
+[VERSE 1]
+Bm
+Kissy face, kissy face
+                    A
+Sent to your phone, but
+Bm
+I'm tryna kiss your lips for real (Uh-huh, uh-huh)
+Bm
+Red hearts, red hearts
+                    A
+That's what I'm on, yeah
+Bm
+Come give me somethin' I can feel, oh-oh, oh
+
+[PRE-CHORUS]
+G                        A
+Don't you want me like I want you, baby?
+Bm                       D
+Don't you need me like I need you now?
+G                     A
+Sleep tomorrow, but tonight, go crazy
+Bm                       N.C.
+All you gotta do is just meet me at the
+
+[CHORUS 1]
+Bm
+Apateu, apateu
+           A
+Apateu, apateu
+Bm
+Apateu, apateu
+Uh, uh-huh, uh-huh
+Bm
+Apateu, apateu
+           A
+Apateu, apateu
+Bm
+Apateu, apateu
+Uh, uh-huh, uh-huh
+
+[VERSE 2]
+         Bm
+It's whatever (Whatever), it's whatever (Whatever)
+                                   A
+It's whatever (Whatever) you like (Woo)
+Bm
+Turn this apateu into a club (Uh-huh, uh-huh)
+            Bm                                           A
+I'm talkin' drink, dance, smoke, freak, party all night (Come on)
+Bm
+Geonbae, geonbae, girl, what's up? Oh-oh, oh
+
+[PRE-CHORUS 2]
+G                        A
+Don't you want me like I want you, baby?
+Bm                       D
+Don't you need me like I need you now?
+G                     A
+Sleep tomorrow, but tonight, go crazy
+Bm                       N.C.
+All you gotta do is just meet me at the
+
+[CHORUS 2]
+G
+Apateu, apateu
+A
+Apateu, apateu
+Bm
+Apateu, apateu
+D
+Uh, uh-huh, uh-huh
+G
+Apateu, apateu
+A
+Apateu, apateu
+Bm
+Apateu, apateu
+D
+Uh, uh-huh, uh-huh
+
+[BRIDGE]
+F#7
+Hey, so now you know the game
+Are you ready?
+Cause I'm comin' to get ya, get ya, get ya
+Bm            A
+Hold on, hold on
+          D   B
+I'm on my way
+Bm                     A
+Yeah, yeah, yeah-yeah, yeah
+          D   B
+I'm on my way
+Bm            A
+Hold on, hold on
+          D   B
+I'm on my way
+Bm                     A
+Yeah, yeah, yeah-yeah, yeah
+          E
+I'm on my way
+
+[PRE-CHORUS 3]
+G                        A
+Don't you want me like I want you, baby?
+Bm                       D
+Don't you need me like I need you now?
+G                     A
+Sleep tomorrow, but tonight, go crazy
+Bm                       D
+All you gotta do is just meet me at the
+
+[FINAL CHORUS]
+G
+Apateu, apateu
+A
+Apateu, apateu
+Bm
+Apateu, apateu
+     D
+Just meet me at the (Uh, uh-huh, uh-huh)
+G
+Apateu, apateu
+A
+Apateu, apateu
+Bm
+Apateu, apateu
+     D
+Just meet me at the (Uh, uh-huh, uh-huh)
+G
+Apateu, apateu
+A
+Apateu, apateu
+Bm
+Apateu, apateu
+     D
+Just meet me at the (Uh, uh-huh, uh-huh)
+G
+Apateu, apateu
+A
+Apateu, apateu
+Bm
+Apateu, apateu
+D
+Uh, uh-huh, uh-huh`
+      },
+      {
+        id: 'count_on_me_bruno_mars_default',
+        title: 'Count On Me (Bruno Mars)',
+        meta: 'Key C | No capo | Beginner',
+        content:
+`[INTRO]
+C
+
+[VERSE 1]
+       C                                              Em
+If you ever find yourself stuck in the middle of the sea
+       Am          G     F
+I'll sail the world to find you
+       C                                                  Em
+If you ever find yourself lost in the dark and you can't see
+      Am         G     F
+I'll be the light to guide you
+
+[PRE-CHORUS]
+Dm                      Em
+Find out what we're made of
+     F                                     G
+When we are called to help our friends in need
+
+[CHORUS]
+        C                Em
+You can count on me like one, two, three
+      Am      G
+I'll be there
+    F
+And I know when I need it
+      C                 Em
+I can count on you like four, three, two
+           Am       G
+And you'll be there
+       F
+Cause that's what friends are s'posed to do
+   C
+Oh yeah
+                 Em
+Ooh ooh ooh ooh ooh
+                 Am  G
+Ooh ooh ooh ooh ooh ooh
+F    G
+Yeah yeah
+
+[VERSE 2]
+          C                                                    Em
+If you're tossin' and you're turnin' and you just can't fall asleep
+      Am        G   F
+I'll sing a song beside you
+           C                                        Em
+And if you ever forget how much you really mean to me
+       Am       G   F
+Every day I will remind you, oh
+
+[PRE-CHORUS 2]
+Dm                      Em
+Find out what we're made of
+     F                                     G
+When we are called to help our friends in need
+
+[CHORUS 2]
+        C                Em
+You can count on me like one, two, three
+      Am      G
+I'll be there
+    F
+And I know when I need it
+      C                 Em
+I can count on you like four, three, two
+           Am       G
+And you'll be there
+       F
+Cause that's what friends are s'posed to do
+   C
+Oh yeah
+                 Em
+Ooh ooh ooh ooh ooh
+                 Am  G
+Ooh ooh ooh ooh ooh ooh
+F    G
+Yeah yeah
+
+[BRIDGE]
+       Dm             Em                Am   G
+You'll always have my shoulder when you cry
+     Dm            Em            F    G
+I'll never let go, never say goodbye, you know you can
+
+[CHORUS / OUTRO]
+C                Em
+Count on me like one, two, three
+      Am      G
+I'll be there
+    F
+And I know when I need it
+      C                 Em
+I can count on you like four, three, two
+              Am     G
+And you'll be there
+       F
+Cause that's what friends are s'posed to do
+   C
+Oh yeah
+                 Em
+Ooh ooh ooh ooh ooh
+                 Am  G
+Ooh ooh ooh ooh ooh ooh
+          F                              C
+You can count on me cause I can count on you`
+      }
+    ]
+  }
+};
